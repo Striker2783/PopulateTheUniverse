91057fbc47c58a9529fefba4584f35dd4c1cdfce
striker2783 <owenxingli@gmail.com>
2024-04-22 11:53:34 -0400

    Rate Class

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@ import Decimal from "break_eternity.js";
 import { Listener } from "./utils/listeners";
 
 let food = new Listener(new Decimal(0), game.food);
+let food_rate = new Listener(new Decimal(0), game.food.rate);
 let total_food = new Listener(new Decimal(0), game.food.total);
 let humans = new Listener(new Decimal(0), game.humans);
 
 onBeforeUnmount(() => {
   food.disconnect();
+  food_rate.disconnect();
   humans.disconnect();
   total_food.disconnect();
 });
@@ -17,6 +19,7 @@ onBeforeUnmount(() => {
 
 <template>
   <h1>Food: {{ food.value }}</h1>
+  <div>Rate {{ food_rate.value.toFixed(2) }}/s</div>
   <button @click="game.increment_food()">Increment</button>
   <h1>Total Food: {{ total_food.value }}</h1>
   <h1>Humans: {{ humans.value }}</h1>
--- src/utils/values.ts
+++ src/utils/values.ts
@@ -54,13 +54,50 @@ export class Totaller extends Observer<Decimal> {
   public set value(v: Decimal) {
     if (v.greaterThan(this.value))
       this._total.value = this._total.value.add(v.minus(this.value));
-    this._value = v;
-    this.callAll();
+    super.value = v;
   }
   public get value() {
     return this._value;
   }
   public get total() {
     return this._total;
+  }
+}
+
+export class Rater extends Totaller {
+  private _max_changes = 5;
+  private values: Array<{ t: number; n: Decimal }> = [];
+  private _rate = new Observer(Decimal.dZero);
+
+  public tick() {
+    if (this.values.length >= this._max_changes) this.values.shift();
+    this.values.push({ t: Date.now(), n: this.value });
+    this.update_rate();
+  }
+
+  private update_rate() {
+    if (this.values.length <= 1) {
+      this.rate.value = Decimal.dZero;
+      return;
+    }
+    const sum_rates = this.values
+      .slice(0, -1)
+      .map((curr, index) => {
+        const next = this.values[index + 1];
+        const diff_time = (next.t - curr.t) / 1000;
+        const diff_num = next.n.minus(curr.n);
+        const rate = diff_num.div(diff_time);
+        return rate;
+      })
+      .reduce((acc, c) => acc.plus(c), Decimal.dZero);
+    this.rate.value = sum_rates.div(this.values.length - 1);
+  }
+
+  public get rate() {
+    return this._rate;
+  }
+
+  public set max_changes(v: number) {
+    this._max_changes = v;
   }
 }
