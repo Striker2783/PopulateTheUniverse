13ee3b370ab53ec75fabe3804cbe31c3d306e796
striker2783 <owenxingli@gmail.com>
2024-04-22 13:23:03 -0400

    Added Wood and Stone
    New tick table.

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,24 @@ import game from "./game/game";
 import Decimal from "break_eternity.js";
 import { Listener } from "./utils/listeners";
 
-let food = new Listener(new Decimal(0), game.food);
-let food_rate = new Listener(new Decimal(0), game.food.rate);
-let humans = new Listener(new Decimal(0), game.humans);
-let human_rate = new Listener(Decimal.dZero, game.humans.rate);
+let food = Listener.new_decimal(game.food);
+let food_rate = Listener.new_decimal(game.food.rate);
+let wood = Listener.new_decimal(game.wood);
+let wood_rate = Listener.new_decimal(game.wood.rate);
+let stone = Listener.new_decimal(game.stone);
+let stone_rate = Listener.new_decimal(game.stone.rate);
+let humans = Listener.new_decimal(game.humans);
+let human_rate = Listener.new_decimal(game.humans.rate);
 
 onBeforeUnmount(() => {
   food.disconnect();
   food_rate.disconnect();
   humans.disconnect();
   human_rate.disconnect();
+  wood.disconnect();
+  wood_rate.disconnect();
+  stone.disconnect();
+  stone_rate.disconnect();
 });
 </script>
 
@@ -24,4 +32,10 @@ onBeforeUnmount(() => {
   <h1>Humans: {{ humans.value }}</h1>
   <div>Rate {{ human_rate.value.toFixed(2) }}/s</div>
   <button @click="game.increment_humans()">Increment</button>
+  <h1>Wood: {{ wood.value }}</h1>
+  <div>Rate {{ wood_rate.value.toFixed(2) }}/s</div>
+  <button @click="game.increment_wood()">Increment</button>
+  <h1>Stone: {{ stone.value }}</h1>
+  <div>Rate {{ stone_rate.value.toFixed(2) }}/s</div>
+  <button @click="game.increment_stone()">Increment</button>
 </template>
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,21 +1,37 @@
-import { Observer, Rater, Totaller } from "@/utils/values";
+import { Rater } from "@/utils/values";
 import Decimal from "break_eternity.js";
 
 export class Game {
+  // #region Properties (4)
+
   private readonly _food: Rater = new Rater(Decimal.dZero);
   private readonly _humans: Rater = new Rater(Decimal.dZero);
+  private readonly _stone = new Rater(Decimal.dZero);
+  private readonly _wood = new Rater(Decimal.dZero);
+
+  /**
+   * Things to call tick()
+   */
+  private readonly raters = [this._food, this._humans, this._stone, this._wood];
+
+  // #endregion Properties (4)
+
+  // #region Constructors (1)
 
   public constructor() {
     setInterval(() => this.tick(), 1000);
   }
 
-  private tick() {
-    this.food.tick();
-    this.humans.tick();
+  // #endregion Constructors (1)
+
+  // #region Public Getters And Setters (8)
+
+  public get food(): Rater {
+    return this._food;
   }
 
-  public increment_food() {
-    this.food = this.food.value.plus(1);
+  public set food(v: Decimal) {
+    this._food.value = v;
   }
 
   public get humans(): Rater {
@@ -26,12 +42,28 @@ export class Game {
     this._humans.value = v;
   }
 
-  public set food(v: Decimal) {
-    this._food.value = v;
+  public get stone(): Rater {
+    return this._stone;
   }
 
-  public get food(): Rater {
-    return this._food;
+  public set stone(v: Decimal) {
+    this.stone.value = v;
+  }
+
+  public get wood(): Rater {
+    return this._wood;
+  }
+
+  public set wood(v: Decimal) {
+    this._wood.value = v;
+  }
+
+  // #endregion Public Getters And Setters (8)
+
+  // #region Public Methods (2)
+
+  public increment_food() {
+    this.food = this.food.value.plus(1);
   }
 
   public increment_humans() {
@@ -39,6 +71,26 @@ export class Game {
     this.humans = this.humans.value.plus(1);
     this.food = this.food.value.minus(1);
   }
+
+  public increment_wood() {
+    this.wood = this.wood.value.plus(1);
+  }
+
+  public increment_stone() {
+    this.stone = this.stone.value.plus(1);
+  }
+
+  // #endregion Public Methods (2)
+
+  // #region Private Methods (1)
+
+  private tick() {
+    this.raters.forEach((field) => {
+      field.tick();
+    });
+  }
+
+  // #endregion Private Methods (1)
 }
 
 export default new Game();
