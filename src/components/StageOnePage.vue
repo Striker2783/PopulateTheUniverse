23b03e72fbc97110182582329c89b8c41b816ef9
striker2783 <owenxingli@gmail.com>
2024-04-23 19:46:14 -0400

    Moved ref into its own getter.

--- src/components/StageOnePage.vue
+++ src/components/StageOnePage.vue
@@ -3,16 +3,16 @@ import game from "@/game/game";
 </script>
 
 <template>
-  <h1>Food: {{ game.resources.get("food")!.value.floor() }}</h1>
-  <div>Rate {{ game.resources.get("food")!.rate.value.toFixed(2) }}/s</div>
+  <h1>Food: {{ game.resources.get("food")!.ref.value.floor() }}</h1>
+  <div>Rate {{ game.resources.get("food")!.rate.ref.value.toFixed(2) }}/s</div>
   <button @click="game.start_increment('food')">Increment</button>
-  <h1>Humans: {{ game.resources.get("human")!.value.floor() }}</h1>
-  <div>Rate {{ game.resources.get("human")!.rate.value.toFixed(2) }}/s</div>
+  <h1>Humans: {{ game.resources.get("human")!.ref.value.floor() }}</h1>
+  <div>Rate {{ game.resources.get("human")!.rate.ref.value.toFixed(2) }}/s</div>
   <button @click="game.start_increment('human')">Increment</button>
-  <h1>Wood: {{ game.resources.get("wood")!.value.floor() }}</h1>
-  <div>Rate {{ game.resources.get("wood")!.rate.value.toFixed(2) }}/s</div>
+  <h1>Wood: {{ game.resources.get("wood")!.ref.value.floor() }}</h1>
+  <div>Rate {{ game.resources.get("wood")!.rate.ref.value.toFixed(2) }}/s</div>
   <button @click="game.start_increment('wood')">Increment</button>
-  <h1>Stone: {{ game.resources.get("stone")!.value.floor() }}</h1>
-  <div>Rate {{ game.resources.get("stone")!.rate.value.toFixed(2) }}/s</div>
+  <h1>Stone: {{ game.resources.get("stone")!.ref.value.floor() }}</h1>
+  <div>Rate {{ game.resources.get("stone")!.rate.ref.value.toFixed(2) }}/s</div>
   <button @click="game.start_increment('stone')">Increment</button>
 </template>
--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -3,8 +3,12 @@ import game from "@/game/game";
 </script>
 
 <template>
-  <h1>Total Food {{ game.resources.get("food")!.total.value.floor() }}</h1>
-  <h1>Total Humans {{ game.resources.get("human")!.total.value.floor() }}</h1>
-  <h1>Total Wood {{ game.resources.get("wood")!.total.value.floor() }}</h1>
-  <h1>Total Stone {{ game.resources.get("stone")!.total.value.floor() }}</h1>
+  <h1>Total Food {{ game.resources.get("food")!.total.ref.value.floor() }}</h1>
+  <h1>
+    Total Humans {{ game.resources.get("human")!.total.ref.value.floor() }}
+  </h1>
+  <h1>Total Wood {{ game.resources.get("wood")!.total.ref.value.floor() }}</h1>
+  <h1>
+    Total Stone {{ game.resources.get("stone")!.total.ref.value.floor() }}
+  </h1>
 </template>
