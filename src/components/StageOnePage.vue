0e206afd5651669843a9f5623b0ce8b013f8ca4d
striker2783 <owenxingli@gmail.com>
2024-04-22 21:22:48 -0400

    Automatic Gathering

--- src/components/StageOnePage.vue
+++ src/components/StageOnePage.vue
@@ -5,14 +5,14 @@ import game from "@/game/game";
 <template>
   <h1>Food: {{ game.resources.get("food")!.value }}</h1>
   <div>Rate {{ game.resources.get("food")!.rate.value.toFixed(2) }}/s</div>
-  <button @click="game.increment('food')">Increment</button>
+  <button @click="game.start_increment('food')">Increment</button>
   <h1>Humans: {{ game.resources.get("human")!.value }}</h1>
   <div>Rate {{ game.resources.get("human")!.rate.value.toFixed(2) }}/s</div>
-  <button @click="game.increment('human')">Increment</button>
+  <button @click="game.start_increment('human')">Increment</button>
   <h1>Wood: {{ game.resources.get("wood")!.value }}</h1>
   <div>Rate {{ game.resources.get("wood")!.rate.value.toFixed(2) }}/s</div>
-  <button @click="game.increment('wood')">Increment</button>
+  <button @click="game.start_increment('wood')">Increment</button>
   <h1>Stone: {{ game.resources.get("stone")!.value }}</h1>
   <div>Rate {{ game.resources.get("stone")!.rate.value.toFixed(2) }}/s</div>
-  <button @click="game.increment('stone')">Increment</button>
+  <button @click="game.start_increment('stone')">Increment</button>
 </template>
--- src/game/game.ts
+++ src/game/game.ts
@@ -2,17 +2,20 @@ import { Rater } from "@/utils/values";
 import Decimal from "break_eternity.js";
 import {
   Resources,
+  type ResourceCost,
   type ResourceData,
   type ResourceNames,
 } from "./data/resources";
 import { Resource } from "./classes/resource";
 
 export class Game {
-  // #region Properties (1)
+  // #region Properties (2)
 
   private readonly _resources: Map<ResourceNames, Resource>;
 
-  // #endregion Properties (1)
+  private current_gathering?: { name: ResourceNames; interval_id: number };
+
+  // #endregion Properties (2)
 
   // #region Constructors (1)
 
@@ -21,7 +24,9 @@ export class Game {
     for (const [key, resource] of Object.entries(Resources)) {
       this._resources.set(key as ResourceNames, new Resource(resource));
     }
-    setInterval(() => this.tick(), 1000);
+    setInterval(() => {
+      this.tick();
+    }, 1000);
   }
 
   // #endregion Constructors (1)
@@ -36,43 +41,65 @@ export class Game {
 
   // #region Public Methods (1)
 
-  public increment(resource_name: ResourceNames) {
-    const resource_data = Resources[resource_name];
-    const resource = this.resources.get(resource_name)!;
-
-    if (resource_data.increment !== undefined && !resource_data.increment.can)
-      return;
-    if (resource.onDelay.value) return;
-
-    const delay =
-      resource_data.increment === undefined
-        ? 1
-        : resource_data.increment!.delay || 1;
-    const cost = resource_data.cost;
-    if (cost === undefined) {
-      resource.value = resource.value.plus(1);
-    } else {
-      for (const [key, value] of Object.entries(cost!)) {
-        if (this.resources.get(key as ResourceNames)!.value.lessThan(value))
-          return;
-      }
-      for (const [key, value] of Object.entries(cost!)) {
-        const needed_resource = this.resources.get(key as ResourceNames)!;
-        needed_resource.value = needed_resource.value.minus(value);
-      }
-      resource.value = resource.value.add(1);
+  public start_increment(resource_name: ResourceNames) {
+    if (this.current_gathering) {
+      clearInterval(this.current_gathering.interval_id);
+      this.current_gathering = undefined;
     }
-
-    if (delay == 0) return;
-    resource.onDelay.value = true;
-    setTimeout(() => {
-      resource.onDelay.value = false;
-    }, delay * 1000);
+    const interval = setInterval(() => {
+      this.get_resource(resource_name);
+    }, this.resources.get(resource_name)!.delay * 1000);
+    this.current_gathering = {
+      interval_id: interval,
+      name: resource_name,
+    };
   }
 
   // #endregion Public Methods (1)
 
-  // #region Private Methods (1)
+  // #region Private Methods (2)
+
+  private can_afford(amount: number, cost?: ResourceCost) {
+    if (!cost) return true;
+    for (const [key, value] of Object.entries(cost!)) {
+      // Can afford single resource
+      if (
+        this.resources
+          .get(key as ResourceNames)!
+          .value.div(amount)
+          .lessThan(value)
+      )
+        return false;
+    }
+    return true;
+  }
+
+  private get_resource(resource_name: ResourceNames, amount: number = 1) {
+    const resource_data = Resources[resource_name];
+    const resource = this.resources.get(resource_name)!;
+
+    if (!resource.canIncrement) return;
+    if (resource.onDelay.value) return;
+
+    const cost = resource_data.cost;
+    if (cost === undefined) {
+      resource.value = resource.value.plus(1);
+    } else {
+      if (!this.can_afford(amount, cost)) return;
+      for (const [key, value] of Object.entries(cost!)) {
+        const needed_resource = this.resources.get(key as ResourceNames)!;
+        needed_resource.value = needed_resource.value.minus(
+          new Decimal(value).times(amount)
+        );
+      }
+      resource.value = resource.value.add(1);
+    }
+
+    if (resource.delay == 0) return;
+    setTimeout(() => {
+      resource.onDelay.value = false;
+    }, resource.delay * 1000 - 100);
+  }
 
   private tick() {
     this._resources.forEach((field) => {
@@ -80,7 +107,7 @@ export class Game {
     });
   }
 
-  // #endregion Private Methods (1)
+  // #endregion Private Methods (2)
 }
 
 export default new Game();
