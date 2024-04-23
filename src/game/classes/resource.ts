c0ed0748cb675577c8375e11c9ee2efc76ba368b
striker2783 <owenxingli@gmail.com>
2024-04-23 09:40:40 -0400

    Refactor Getting Resources

--- src/game/classes/resource.ts
+++ src/game/classes/resource.ts
@@ -4,23 +4,22 @@ import Decimal from "break_eternity.js";
 
 export class Resource extends Rater {
   private readonly data;
-  private _onDelay = new Observer(false);
-  public get onDelay() {
-    return this._onDelay;
-  }
-  public set onDelay(value) {
-    this._onDelay = value;
-  }
   public constructor(resource_data: ResourceData) {
     super(new Decimal(resource_data.default_value || 0));
     this.data = resource_data;
   }
 
-  public get canIncrement() {
-    return this.data.increment ? this.data.increment.can : true;
-  }
-
   public get delay() {
     return this.data.increment ? this.data.increment.delay || 1 : 1;
+  }
+
+  public get cost() {
+    return this.data.cost || {};
+  }
+
+  public get can_increment() {
+    if (!this.data.increment) return true;
+    if (this.data.increment.can === undefined) return true;
+    return this.data.increment.can;
   }
 }
