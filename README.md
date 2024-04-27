cf4c4ddeea39b077a53b5febfc61f6aefec1f2b7
Striker2783 <owenxingli@gmail.com>
2024-04-27 16:04:20 -0400

    Initialization

---
+++ README.md
@@ -0,0 +1,15 @@
+# Populate The Universe
+
+This is an incremental game whose goal is to populate humanity throughout the universe. It will include many different stages starting from primitive stage to universe stage.
+
+## Project Setup
+
+```sh
+pnpm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+pnpm dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "game": fileURLToPath(new URL("./game", import.meta.url))
+    },
+  },
+  base: "https://striker2783.github.io/PopulateTheUniverse",
+});
