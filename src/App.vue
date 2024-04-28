ef05770430b9dd18bfc8ce38118c04fa9a3e0b6f
Striker2783 <owenxingli@gmail.com>
2024-04-27 22:55:27 -0400

    CSS Styling

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,48 @@ const ResearchStuff = ref(Researchs)
 </script>
 
 <template>
-    <h1>Humans: {{ game.humans.v.r.value.toFixed(2) }} / {{ game.humans.m.r.value.toFixed(2) }}</h1>
-    <p>Rate: {{ game.human_rate.toFixed(2) }}/s</p>
-    <div v-if="game.unlocks.CrudeHouse">
-        <h1>Land: {{ game.land.left.toFixed(2) }} / {{ game.land.m.r.value.toFixed(2) }}</h1>
-        <h1>Crude Huts: {{ game.crude_homes.v.toFixed(2) }}</h1>
-        <button @click="game.build(1, 'crude_homes')">Build</button>
-        <button @click="game.build(-1, 'crude_homes')">Destroy</button>
-    </div>
-    <div v-if="game.unlocks.BasicAgriculture">
-        <h1>Farm: {{ game.farms.v.toFixed(2) }}</h1>
-        <button @click="game.build(1, 'farms')">Build</button>
-        <button @click="game.build(-1, 'farms')">Destroy</button>
-    </div>
-    <h1>Research: {{ game.research_points.r.value.toFixed(2) }}</h1>
-    <p>Rate: {{ game.research_rate.toFixed(2) }}/s</p>
-    <ul v-for="(upgrade, k) in ResearchStuff">
-        <button @click="game.research(k)">
-            <p style="font-size: 1.2em;">
-                {{ upgrade.name }}
-            </p>
-            <p>
-                {{ upgrade.description }}
-            </p>
-            <div v-if="game.researched[k]">
-                <p>Bought</p>
-            </div>
-            <div v-else>
-                <p>Cost:</p>
-                <p>{{ upgrade.cost_display }}</p>
-            </div>
-        </button>
+    <ul class="bar">
+        <li class="resource-item">
+            <p>Humans: {{ game.humans.v.r.value.toFixed(2) }} / {{ game.humans.m.r.value.toFixed(2) }}</p>
+            <p>Rate: {{ game.human_rate.toFixed(2) }}/s</p>
+        </li>
+        <li class="resource-item">
+            <p>Research: {{ game.research_points.r.value.toFixed(2) }}</p>
+            <p>Rate: {{ game.research_rate.toFixed(2) }}/s</p>
+        </li>
+    </ul>
+    <ul class="bar">
+        <li>
+            <p>Land: {{ game.land.left.toFixed(2) }} / {{ game.land.m.r.value.toFixed(2) }}</p>
+        </li>
+        <li v-if="game.unlocks.CrudeHouse">
+            <p>Crude Huts: {{ game.crude_homes.v.toFixed(2) }}</p>
+            <button @click="game.build(1, 'crude_homes')">Build</button>
+            <button @click="game.build(-1, 'crude_homes')">Destroy</button>
+        </li>
+        <li v-if="game.unlocks.BasicAgriculture">
+            <p>Farm: {{ game.farms.v.toFixed(2) }}</p>
+            <button @click="game.build(1, 'farms')">Build</button>
+            <button @click="game.build(-1, 'farms')">Destroy</button>
+        </li>
+    </ul>
+    <ul class="upgrades">
+        <li v-for="(upgrade, k) in ResearchStuff">
+            <button class="upgrade-button" @click="game.research(k)" :disabled="game.researched[k]">
+                <p class="upgrade-name">
+                    {{ upgrade.name }}
+                </p>
+                <p class="upgrade-description">
+                    {{ upgrade.description }}
+                </p>
+                <div v-if="game.researched[k]">
+                    <p>Bought</p>
+                </div>
+                <div class="upgrade-cost" v-else>
+                    <p>Cost:</p>
+                    <p>{{ upgrade.cost_display }}</p>
+                </div>
+            </button>
+        </li>
     </ul>
 </template>
